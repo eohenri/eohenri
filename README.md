6315df8b3b478ddc250c20691b7e6ec2897d6824
eohenri <151474844+eohenri@users.noreply.github.com>
2024-02-23 23:17:46 -0300

    Update and rename README.md to me.js

--- README.md
+++
@@ -1,1 +0,0 @@
-¡ey! Soy Henri, desarrollador de bots para Discord. ¡Soy el desarrollador de Star Gaby, un simple bot multifuncional que evoluciona cada vez más!
---
+++ me.js
@@ -0,0 +1,2 @@
+Hi, my name is Henri, I'm a simple bot programmer for discord.
+I'm one of two developers at [Star Gaby](https://discord.com/application-directory/1108562673899143168) need help with something involving Gaby? Enter the official [support] server (https://discord.com/invite/533XETUMgp) to receive assistance directly from the Star Gaby support team.
